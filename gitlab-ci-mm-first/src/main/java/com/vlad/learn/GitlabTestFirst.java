ab0dcaaf50b8019defe6a893f628267bfe13ff3c
vladyslav_sereda1 <vladyslav.sereda@LYV.livenation.com>
2019-11-21 15:49:46 +0200

    Third change to first

--- gitlab-ci-mm-first/src/main/java/com/vlad/learn/GitlabTestFirst.java
+++ gitlab-ci-mm-first/src/main/java/com/vlad/learn/GitlabTestFirst.java
@@ -2,7 +2,7 @@ package com.vlad.learn;
 
 public class GitlabTestFirst {
 
-    private String hello = "hello first-2";
+    private String hello = "hello first-3";
 
     public String getHello() {
         return hello;
