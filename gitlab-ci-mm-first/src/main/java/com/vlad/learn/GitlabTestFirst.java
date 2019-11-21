f8a1bf4072a025ad637ab8057b9ac09b5c2f9517
vladyslav_sereda1 <vladyslav.sereda@LYV.livenation.com>
2019-11-21 15:32:30 +0200

    Add trigger on change rule

--- gitlab-ci-mm-first/src/main/java/com/vlad/learn/GitlabTestFirst.java
+++ gitlab-ci-mm-first/src/main/java/com/vlad/learn/GitlabTestFirst.java
@@ -2,7 +2,7 @@ package com.vlad.learn;
 
 public class GitlabTestFirst {
 
-    private String hello = "hello first";
+    private String hello = "hello first-1";
 
     public String getHello() {
         return hello;
